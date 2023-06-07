bc426ac9749975b342ec58cb0796c93f5800122c
Christophe Jollivet <jollivet@apside.fr>
2023-06-07 14:01:18 +0200

    Observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DemoObservableService } from '../common/demo-observable.service';
+import { map, take } from 'rxjs';
 
 @Component({
   selector: 'crm-home',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  constructor(private demoObservable:DemoObservableService){}
+
+  testObservable():void{
+    console.log("before")
+    this.demoObservable.getObservable()
+      .pipe(
+        map(x=>x*2),
+        take(2)
+      ).subscribe({
+          next:(data:number)=>{console.log(data)},
+          error:(error:Error)=>{console.error(error)},
+          complete:()=>{console.log('complete')}
+    })
+    console.log('after')
+  }
 }
