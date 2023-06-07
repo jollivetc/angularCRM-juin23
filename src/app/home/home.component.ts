0abdd8ecf66baef3673ad93fd59015653790f0d7
Christophe Jollivet <jollivet@apside.fr>
2023-06-07 14:57:22 +0200

    Observable end

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,27 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { DemoObservableService } from '../common/demo-observable.service';
-import { map, take } from 'rxjs';
+import { Observable, Subscriber, Subscription, catchError, map, of, take } from 'rxjs';
 
 @Component({
   selector: 'crm-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy{
+
+  obs?:Observable<number>;
+  private subs:Subscription[]=[];
 
   constructor(private demoObservable:DemoObservableService){}
 
+  ngOnDestroy(): void {
+    this.subs.forEach(sub=>sub.unsubscribe());
+  }
+
   testObservable():void{
     console.log("before")
-    this.demoObservable.getObservable()
-      .pipe(
-        map(x=>x*2),
-        take(2)
-      ).subscribe({
+    const subscriber = {
           next:(data:number)=>{console.log(data)},
           error:(error:Error)=>{console.error(error)},
           complete:()=>{console.log('complete')}
-    })
+    }
+
+    const subscription:Subscription = this.demoObservable.getObservable()
+      .pipe(
+        map(x=>x*2),
+        take(5)
+      ).subscribe(subscriber)
+    this.subs.push(subscription);
     console.log('after')
+  }
+
+  testObservableAsync():void{
+    this.obs = this.demoObservable.getObservable()
+        .pipe(
+          map(x=>x*10),
+          catchError(error=>of(error))
+        )
   }
 }
